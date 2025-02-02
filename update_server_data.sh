66773890d18c6ddc2c5c08b2a3fbd5ca67c30e97
maxazure <maxazure@gmail.com>
2025-02-02 22:54:45 +1300

    添加更新脚本执行权限

--- update_server_data.sh
+++ update_server_data.sh
@@ -7,8 +7,12 @@ scp instance/links.db www@home:/www/wwwroot/home.jayliu.co.nz/instance/
 ssh www@home << 'EOF'
   cd /www/wwwroot/home.jayliu.co.nz
   git pull
-  docker compose down
-  docker compose up -d
+EOF
+
+ssh maxazure@home << 'EOF'
+  cd /www/wwwroot/home.jayliu.co.nz
+  sudo docker compose down
+  sudo docker compose up -d
 EOF
 
 echo "服务器更新完成！"
