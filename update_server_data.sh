83742249a388b5abd5eaa11151301db348bbaacf
maxazure <maxazure@gmail.com>
2025-02-02 22:53:15 +1300

    增强更新脚本：添加远程服务器更新命令

--- update_server_data.sh
+++ update_server_data.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+# 上传数据库文件
 scp instance/links.db www@home:/www/wwwroot/home.jayliu.co.nz/instance/
+
+# 远程执行更新命令
+ssh www@home << 'EOF'
+  cd /www/wwwroot/home.jayliu.co.nz
+  git pull
+  docker compose down
+  docker compose up -d
+EOF
+
+echo "服务器更新完成！"
